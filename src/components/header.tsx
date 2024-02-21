f2b2861d9bdf2a53e1843ed519ecb05145ae94a4
Aleksey Verin <verevaa@gmail.com>
2024-02-21 15:46:56 +0300

    feat: header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,11 +5,10 @@ const Header = () => {
     <header className='font-display'>
       <div className='max-w-screen-xl p-4 h-full m-auto flex items-center justify-between '>
         <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>
-          Чудо-генератор приветствия Андрея
+          Генератор случайных Маркичей
         </h1>
         <div className='w-10 flex items-center justify-center ml-2'>
-
-        <ModeToggle />
+          <ModeToggle />
         </div>
       </div>
     </header>
